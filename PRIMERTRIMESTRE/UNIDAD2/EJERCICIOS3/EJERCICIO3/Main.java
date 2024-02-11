ebac19a3376af6bae0af2e2e5cbf04ef234cefcb
Angeeel04 <angelgabe0708@gmail.com>
2024-02-11 20:26:30 +0100

    modificando

--- PRIMERTRIMESTRE/UNIDAD2/EJERCICIOS3/EJERCICIO3/Main.java
+++
@@ -1,8 +0,0 @@
-class Main{
-    public static void main(String args[]) {
-        int miEdad = 19;
-        char miNombre = 'A' ;
-        
-        System.out.println("Mi edad es: " + miEdad + " y mi nombre es: " + miNombre);
-    }
-}
